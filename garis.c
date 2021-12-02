298f2542932dc572cb02952438dd9cc72b20b63f
Rizky Ramadhana Putra Kusnaryanto <65774061+rizkyramadhana26@users.noreply.github.com>
2021-12-02 21:49:41 +0700

    Update garis.c

--- garis.c
+++ garis.c
@@ -11,34 +11,33 @@ Kamis, 2 September 2021
 
 
 /* ***************************************************************** */
-/* DEFINISI PRIMITIF                                                 */
+/* PRIMITIVE DEFINITION                                              */ 
 /* ***************************************************************** */
-/* *** Konstruktor membentuk GARIS *** */
+/* *** Constructor to make GARIS *** */
 void MakeGARIS (POINT P1, POINT P2, GARIS * L){
-/* I.S. P1 dan P2 terdefinisi */
-/* F.S. L terdefinisi dengan L.PAw = P1 dan L.PAkh = P2 */
-/* Membentuk sebuah L dari komponen-komponennya */
+/* I.S. P1 and P2 are defined */
+/* F.S. L defined with L.PAw = P1 and L.PAkh = P2 */
+/* Build L based one its component */
     PAwal(*L)=P1;
     PAkhir(*L)=P2;
 }
 /* ***************************************************************** */
-/* KELOMPOK INTERAKSI DENGAN I/O DEVICE, BACA/TULIS                  */
+/* READ/WRITE                                                        */
 /* ***************************************************************** */
 void BacaGARIS (GARIS * L){
-/* I.S. sembarang */
-/* F.S. L terdefinisi sebagai GARIS yang valid */
-/* Proses : mengulangi membaca dua buah nilai P1 dan P2 sehingga dapat
-   membentuk GARIS yang valid
-   GARIS disebut valid jika titik awal tidak sama dengan titik akhirnya. */
-/* Jika pembacaan garis tidak valid, diberikan pesan kesalahan:
+/* I.S. anything */
+/* F.S. L defines as valid GARIS */
+/* Process : reading two points until it makes a valid GARIS 
+   GARIS is valid if the first point is not equal with the second point */
+/* If failed, print error message: 
    "Garis tidak valid" */
-/* Contoh:
+/* Example:
    2 3
    2 3
    Garis tidak valid
    2 3
-   3 3
-   --> terbentuk garis L dengan L.PAw = <2,3> dan L.PAkh = <3,3> */
+   3 3 
+   --> L is formed with L.PAw = <2,3> and L.PAkh = <3,3> */
    //Kamus
    POINT p1,p2;
    boolean valid=false;
@@ -55,10 +54,8 @@ void BacaGARIS (GARIS * L){
 }
 
 void TulisGARIS (GARIS L){
-/* I.S. L terdefinisi */
-/* F.S. L tertulis di layar dengan format "((x1,y1),(x2,y2))"
-   tanpa tambahan karakter apa pun di depan, di tengah, atau di belakang,
-   termasuk spasi dan enter */
+/* I.S. L is defined */
+/* F.S. L is printed with format "((x1,y1),(x2,y2))"  */
    //Kamus
    float x1,x2,y1,y2;
    //Algoritma
@@ -70,10 +67,9 @@ void TulisGARIS (GARIS L){
 
 }
 
-/* *** Kelompok operasi garis *** */
+/* *** Operation between GARIS *** */
 float PanjangGARIS (GARIS L){
-/* Menghitung panjang garis L : berikan rumusnya */
-/*rumusnya adalah sqrt((x2-x1)^2 + (y2-y1)^2)*/
+/* return the length of a GARIS */
     //Kamus
     float selisihX, selisihY;
     //Algoritma
@@ -83,32 +79,28 @@ float PanjangGARIS (GARIS L){
 
 }
 float Gradien (GARIS L){
-/* Mengembalikan Gradien (m) dari L */
-/* Gradien garis yang melalui (x1,y1) dan (x2,y2) adalah: (y2-y1)/(x2-x1) */
-/* Prekondisi : x1 != x2 */
-/*Asumsi prekondisi selalu benar*/
+/* return the gradient (m) of L */
+/* Gradient of a line that pass through (x1,y1) and (x2,y2) is: (y2-y1)/(x2-x1) */
+/* Precondition : x1 != x2 */
     return (Ordinat(PAkhir(L))-Ordinat(PAwal(L)))/(Absis(PAkhir(L))-Absis(PAwal(L)));
 }
 
 void GeserGARIS (GARIS * L, float deltaX, float deltaY){
-/* I.S. L terdefinisi */
-/* F.S. L digeser sebesar deltaX ke arah sumbu X dan sebesar deltaY ke arah sumbu Y */
-/* Proses : PAw dan PAkh digeser. */
+/* I.S. L is defined */
+/* F.S. L is translated deltaX parallel with X axes and deltaY parallel with Y axes */
     PAwal(*L)= PlusDelta(PAwal(*L), deltaX, deltaY);
     PAkhir(*L)= PlusDelta(PAkhir(*L), deltaX, deltaY);
 }
 
 
-/* *** Kelompok predikat *** */
+/* *** Predicate *** */
 boolean IsTegakLurus (GARIS L1, GARIS L2){
-/* Menghasilkan true jika L1 tegak lurus terhadap L2 */
-/* Dua garis saling tegak lurus jika hasil perkalian kedua gradiennya = -1 */
+/* Return true if two lines are perpendicular*/
     return fabs(Gradien(L1)*Gradien(L2)+1)<1e-5;
 
 }
 
 boolean IsSejajar (GARIS L1, GARIS L2){
-/* Menghasilkan true jika L "sejajar" terhadap L1 */
-/* Dua garis saling sejajar jika memiliki gradien yang sama */
+/* Return true if two lines are parallel*/
     return fabs(Gradien(L1)-Gradien(L2))<1e-5;
 }
